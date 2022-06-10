d45629dcf028ea965f4a3d231948941d1edf7e68
jiaqianzhang <jiaqianzhang@microsoft.com>
2022-06-10 12:12:42 +0800

    commit from idea

--- src/main/java/com/xxx/jiaqianzhang/test.java
+++ src/main/java/com/xxx/jiaqianzhang/test.java
@@ -11,5 +11,6 @@ public class test {
         System.out.println("hello git6 from master,push to github");
         System.out.println("hello git7 from master,from local idea");
         System.out.println("hello git7 from master,from github web");
+        System.out.println("hello git8 from master,from github web");
     }
 }
