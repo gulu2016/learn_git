4a778ca37786a3572b6677c9340a44b542c841c8
jiaqianzhang <jiaqianzhang@microsoft.com>
2022-06-10 11:45:49 +0800

    commit from idea hot-fix branch

--- src/main/java/com/xxx/jiaqianzhang/test.java
+++ src/main/java/com/xxx/jiaqianzhang/test.java
@@ -8,5 +8,6 @@ public class test {
         System.out.println("hello git3 from hot-fix");
         System.out.println("hello git5 from master");
         System.out.println("hello git5 from hot-fix");
+        System.out.println("hello git6 from master,push to github");
     }
 }
